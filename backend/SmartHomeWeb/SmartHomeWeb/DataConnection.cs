7a6507746c759adbd4c3f222a95bbe8f102f2310
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2016-02-18 11:49:39 +0100

    Oops, forgot DataConnection

---
+++ backend/SmartHomeWeb/SmartHomeWeb/DataConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace SmartHomeWeb
+{
+    public class DataConnection
+    {
+        const string DatabasePath = "../database/smarthomeweb.db";
+        const string ConnectionString = "Data Source=" + DatabasePath;
+        private SQLiteConnection sqlite;
+
+        public DataConnection()
+        {
+            sqlite = new SQLiteConnection(ConnectionString);
+        }
+    }
+}
