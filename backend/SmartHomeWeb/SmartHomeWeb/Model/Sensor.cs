558b749541e28bbc72c9c56cc9b612d971dd3d61
jonathanvdc <jonathan.vdc@outlook.com>
2016-02-21 15:03:32 +0100

    Created Sensor and HasLocation classes

---
+++ backend/SmartHomeWeb/SmartHomeWeb/Model/Sensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AsyncPoco;
+
+namespace SmartHomeWeb
+{
+	[PrimaryKey("id", autoIncrement = true)]
+	public class Sensor
+	{
+		public Sensor()
+		{
+		}
+
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("name")]
+		public string Name { get; set; }
+
+		[Column("locationId")]
+		public string LocationId { get; set; }
+	}
+}
+
