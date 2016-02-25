a50f639dcbfe4874fdd6f76c8a1cc7acc1412b34
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2016-02-25 13:33:30 +0100

    Expand DB model a little
    Add friendships, sensor tags, data points, and events.

--- backend/database/smarthomeweb.sql
+++ backend/database/smarthomeweb.sql
@@ -1,11 +1,20 @@
+-- In the database, everything is stored in kW and kWh.
+
 create table Person (
     id integer primary key autoincrement,
     name text not null
 );
 
+create table Friendship (
+    person1 integer not null references Person(id),
+    person2 integer not null references Person(id),
+    primary key (person1, person2)
+);
+
 create table Location (
     id integer primary key autoincrement,
-    name text not null
+    name text not null,
+    electricityCost real -- in EUR/(kWh)
 );
 
 create table HasLocation (
@@ -17,7 +26,29 @@ create table HasLocation (
 create table Sensor (
     id integer primary key autoincrement,
     name text not null,
+    description text,
+    notes text,
     locationId integer not null references Location(id)
+);
+
+create table SensorTag (
+    sensorId integer not null references Sensor(id),
+    tag text not null,
+    primary key (sensorId, tag)
+);
+
+create table SensorDataPoint (
+    unixTime integer not null,
+    dataValue integer not null,
+    sensorId integer not null references Sensor(id),
+    primary key (sensorId, unixTime)
+);
+
+create table SensorEvent (
+    unixTime integer not null,
+    description text not null,
+    sensorId integer not null references Sensor(id),
+    primary key (sensorId, unixTime)
 );
 
 insert into Person(name) values
