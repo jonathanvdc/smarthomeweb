176b4385359c5c27845546bfb5890782b3871703
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2016-03-02 20:03:03 +0100

    Write a base API module
    GETs should be very easy to write, at least.

--- backend/SmartHomeWeb/SmartHomeWeb/Modules/API/APIPersonsModule.cs
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/APIPersonsModule.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SmartHomeWeb.Modules.API
 {
-    public class ApiPersonsModule : Nancy.NancyModule
+    public class ApiPersonsModule : ApiModule
     {
         public ApiPersonsModule() : base("api/persons")
         {
-            Get["/"] = _ => Nancy.HttpStatusCode.NotImplemented;
+            ApiGet("/", (_, dc) => dc.GetPersonsAsync());
+            ApiGet("/{id}/", (p, dc) => dc.GetPersonByIdAsync((int) p["id"]));
         }
     }
 }
---
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Nancy.NegotiatorExtensions;
+using Newtonsoft.Json;
+
+namespace SmartHomeWeb.Modules.API
+{
+
+    public abstract class ApiModule : Nancy.NancyModule
+    {
+        protected ApiModule(string modulePath) : base(modulePath) { }
+
+        /// <summary>
+        /// Turn an operation on a parameter list and a data connection (which
+        /// queries a value of type <typeparamref name="T"/> from the database)
+        /// into an async API route handler that performs this operation and
+        /// responds with query result serialized as a JSON object.
+        /// </summary>
+        /// <typeparam name="T">The query's result type.</typeparam>
+        /// <param name="operation">The operation to wrap around.</param>
+        /// <returns>A route handler.</returns>
+        protected Func<dynamic, CancellationToken, Task<dynamic>> Ask<T>(
+            Func<dynamic, DataConnection, Task<T>> operation)
+        {
+            return async (parameters, ct) =>
+            {
+                T result = await DataConnection.Ask<T>(dc => operation(parameters, dc));
+                var json = JsonConvert.SerializeObject(result);
+            
+                var statusCode = result == null
+                    ? Nancy.HttpStatusCode.NotFound
+                    : Nancy.HttpStatusCode.Accepted;
+
+                Nancy.Response response = json;
+                response.ContentType = "application/json";
+                response.StatusCode = statusCode;
+                return response;
+            };
+        }
+
+        /// <summary>
+        /// Register an API GET operation: <code>ApiGet(path, f)</code> is
+        /// short for <code>Get[path, true] = Ask(f)</code>.
+        /// </summary>
+        /// <typeparam name="T">The query's result type.</typeparam>
+        /// <param name="path">The route path to register at.</param>
+        /// <param name="operation">The operation to wrap around.</param>
+        protected void ApiGet<T>(string path, Func<dynamic, DataConnection, Task<T>> operation)
+        {
+            Get[path, true] = Ask(operation);
+        }
+    }
+}
