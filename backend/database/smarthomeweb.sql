5ff32bc799f37c7fa162238aa095ae338ef7a7f0
Pieter Hendriks <mitarohmar@gmail.com>
2016-03-03 09:34:19 +0100

    Added new database layout. Still needs to be finalized. CSV parser copied into master, haven't adapted it to new DB layout, waiting for finalisation. More DB changes will happen, probably.

--- backend/database/smarthomeweb.sql
+++ backend/database/smarthomeweb.sql
@@ -1,67 +1,122 @@
--- In the database, everything is stored in kW and kWh.
-
 create table Person (
-    id integer primary key autoincrement,
-    name text not null
+	id integer primary key autoincrement,
+	username text not null,
+	name text,
+	password text not null, --obviously niet plaintext
+	--users require logging in, non-user persons shouldn't be stored so password and username are required.
+	--username can differ from actual name, actual name can be null.
+	--If no name is specified, username will be displayed in social aspects.
+	
+	--Assignment describes selecting users through address and such, so we will store additional info:
+	birthdate integer not null, --in unixtime
+	address text not null, --Street and number
+	city text not null,
+	zipcode text not null --Not all countries have number-only zip codes.
+	
+	
+	--Can probably be expanded further, I don't know what other details would be useful though.
+	
+);
+create table Friends (
+	personOne integer not null references Person(id),
+	personTwo integer not null references Person(id),
+	primary key (personOne, personTwo) --How do we enforce that the reversed pair is not present?
 );
 
-create table Friendship (
-    person1 integer not null references Person(id),
-    person2 integer not null references Person(id),
-    primary key (person1, person2)
+create table Message (
+	id integer primary key not null autoincrement,
+	sender integer not null references Person(id),
+	recipient integer not null references Person(id),
+	message text not null
+	--'usage messages' as described in assignment can simply insert a link into message,
+	--Though perhaps allowing an attached link is better? (Optional, obviously)
+	--link text
 );
 
 create table Location (
-    id integer primary key autoincrement,
-    name text not null,
-    electricityCost real -- in EUR/(kWh)
+	id integer primary key autoincrement,
+	name text not null
 );
 
+--Reverse index? See example @ hourAverages
+
 create table HasLocation (
-    personId integer not null references Person(id),
-    locationId integer not null references Location(id),
-    primary key (personId, locationId)
+	personId integer not null references Person(id),
+	locationId integer not null references Location(id)
+	primary key (personId, locationId)
 );
 
 create table Sensor (
-    id integer primary key autoincrement,
-    name text not null,
-    description text,
-    notes text,
-    locationId integer not null references Location(id)
+	id integer primary key autoincrement,
+	locationid integer not null references Location(id),
+	title text not null, 
+	description text not null,
+	notes text
+);
+
+create table electricityPrice (
+	locationId integer not null references Location(id), --One location has one price.
+	--Restriction on users should be done through HasLocation table, no point doing that again here.
+	price real
+);
+
+--Might wanna think about reverse index here too, see example @ hourAverage
+create table Tag (
+	id integer primary key autoincrement,
+	name text not null
 );
 
 create table SensorTag (
-    sensorId integer not null references Sensor(id),
-    tag text not null,
-    primary key (sensorId, tag)
+	sensorId integer not null references Sensor(id),
+	tagId integer not null references Tag(id)
 );
 
-create table SensorDataPoint (
-    unixTime integer not null,
-    dataValue integer not null,
-    sensorId integer not null references Sensor(id),
-    primary key (sensorId, unixTime)
+create table Measurements (
+	sensorId integer not null references Sensor(id),
+	unixtime integer not null, --solves timezone issues etc
+	measured real not null,
+	notes text,
+	primary key (sensorId, unixtime)
 );
 
-create table SensorEvent (
-    unixTime integer not null,
-    description text not null,
-    sensorId integer not null references Sensor(id),
-    primary key (sensorId, unixTime)
+--Adding indexes on (unixtime, sensorId) would probably be useful in these tables, to optimize the other search case.
+--example:
+--	create unique index hourIndex ON hourAverage (unixtime, sensorId)
+create table hourAverage (
+	sensorId integer not null references Sensor(id),
+	unixtime integer not null, --unix time for the hour, YYYY-MM-DD XX:00:00, where xx in 0-23 (inclusive).
+	average real not null,
+	notes text, --Notes carry over from measurements, will become CSV in case of multiple. 
+	--Each comment will be enclosed in double quotes for clear seperation in case commas are present. 
+	--Double quotes should become illegal characters in the comment though, 
+	--to ensure no double quotes are present any place but begin and end of a note. 
+	--Important for parsing the notes back into the frontend.
+	--Enclosing in quotes is unnecessary  in the Measurements table they are listed per minute and every user
+	--that writes multiple notes in a minute is just being unreasonable.
+	primary key (sensorId, unixtime)
+
 );
 
-insert into Person(name) values
-    ("Jon Sneyers"), ("Bart Goethals");
+--Day averages are needed seperately because hours don't get the outliers pruned, day does.
+--More than this shouldn't be needed, we can simply grab the records from day table and compute as needed.
+create table dayAverages (
+	sensorId integer not null references Sensor(id),
+	unixtime integer not null, --unix time for YYYY-MM-DD 00:00. Identifies the day.
+	average real not null,
+	notes text,
+	primary key (sensorId, unixtime)
+);
 
-insert into Location(name) values
-    ("De G0.10");
 
-insert into HasLocation(personId, locationId)
-    select Person.id, Location.id
-    from Person, Location;
+--Adding extra for the longer time periods might be advisable though, to save on computing time. 
+--Requires extra storage, but seems reasonable to include yearly, for example.
 
-insert into Sensor(name, locationId)
-    select "Sensor 1", id
-    from Location
-    where name = "De G0.10";
+create table yearAverages (
+	sensorId integer not null references Sensor(id),
+	unixtime integer not null, --unix time identifying the year; YYYY-01-01 00:00. only YYYY can vary.
+	average real not null,
+	notes text,
+	primary key (sensorId, unixtime)
+);
+
+
