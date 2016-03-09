6624bb94b14d39e8a04c32d4f2c8be966ae91c04
jonathanvdc <jonathan.vdc@outlook.com>
2016-03-09 17:30:45 +0100

    Created a database initialization script

---
+++ init-db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Create the database
+pushd backend/database
+chmod +x ./create-db.sh
+./create-db.sh
+popd
+
+# Start the server (assuming that it has been compiled already)
+./backend/SmartHomeWeb/SmartHomeWeb/bin/Debug/SmartHomeWeb.exe &
+serverid=$!
+
+sleep 1
+
+# Post some same data
+curl -X POST -d @example-files/personData.json localhost:8088/api/persons
+
+kill $serverid
