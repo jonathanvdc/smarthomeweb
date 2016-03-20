ea615d0f8f2c57682286d15a2b112a2bb2efa29e
KenB <kenbauwens@hotmail.com>
2016-03-20 17:41:13 +0100

    Added class to pass both location and its sensors to model
    Not sure if this implementation is good but used it to get a working
    result.

---
+++ backend/SmartHomeWeb/libsmarthomeweb/Model/Locationextended.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeWeb.Model
+{
+    public class Locationextended
+    {
+        public List<Sensor> sensoren { get; private set; }
+
+        public Locationextended(Location location)
+        {
+            this.location = location;
+            this.sensoren = new List<Sensor>();
+        }
+
+        public void addsensor(Sensor sensor)
+        {
+            this.sensoren.Add(sensor);
+        }
+
+        public Location location { get; private set; }
+
+
+    }
+}
