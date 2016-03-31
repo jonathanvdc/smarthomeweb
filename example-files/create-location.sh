2252457cff1a975a2e8a640c7443b326f07cfbd1
jonathanvdc <jonathan.vdc@outlook.com>
2016-03-31 19:20:26 +0200

    Explicitly use mono in scripts

--- example-files/create-location.sh
+++ example-files/create-location.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-jsonfield="../json-utils/json-field/bin/Release/json-field.exe"
-jsonadd="../json-utils/json-add/bin/Release/json-add.exe"
-jsonpack="../json-utils/json-pack/bin/Release/json-pack.exe"
-jsonfind="../json-utils/json-find/bin/Release/json-find.exe"
-jsonflatten="../json-utils/json-flatten/bin/Release/json-flatten.exe"
-
-chmod +x $jsonfield
-chmod +x $jsonadd
-chmod +x $jsonpack
-chmod +x $jsonfind
-chmod +x $jsonflatten
+jsonfield="mono ../json-utils/json-field/bin/Release/json-field.exe"
+jsonadd="mono ../json-utils/json-add/bin/Release/json-add.exe"
+jsonpack="mono ../json-utils/json-pack/bin/Release/json-pack.exe"
+jsonfind="mono ../json-utils/json-find/bin/Release/json-find.exe"
+jsonflatten="mono ../json-utils/json-flatten/bin/Release/json-flatten.exe"
 
 curl -X GET localhost:8088/api/persons | $jsonflatten > tmp/persons.json
 guid="$(cat tmp/persons.json | $jsonfind username "$2" | $jsonfield personGuid)"
