eb0774a5cc291354b30f0ea8b06aff37a96b8360
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-04 14:09:22 +0200

    Created a day-average REST API

---
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiDayAverageModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SmartHomeWeb.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartHomeWeb.Modules.API
+{
+    public class ApiDayAverageModule : AggregatedMeasurementModuleBase
+    {
+        public ApiDayAverageModule() : base("api/day-average")
+        { }
+
+        public override TimeSpan TimeQuantum
+        {
+            get { return TimeSpan.FromDays(1); }
+        }
+
+        public override Task<Measurement> GetAggregatedAsync(DataConnection Connection, int SensorId, DateTime Time)
+        {
+            return Connection.GetDayAverageAsync(SensorId, Time);
+        }
+    }
+}
