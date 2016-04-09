643e5b489f927d07f659144cb6100d2123871774
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-09 18:33:07 +0200

    Oops. Forgot to commit a file

---
+++ backend/SmartHomeWeb/libsmarthomeweb/Model/FriendsState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SmartHomeWeb
+{
+	/// <summary>
+	/// Describes possible states for a 'friends' relationship.
+	/// </summary>
+	public enum FriendsState
+	{
+		/// <summary>
+		/// Persons one and two are not friends.
+		/// </summary>
+		None = 0,
+		/// <summary>
+		/// Person one has sent a friend request to person two.
+		/// </summary>
+		FriendRequestSent = 1,
+		/// <summary>
+		/// Person two has sent a friend request to person one.
+		/// </summary>
+		FriendRequestRecieved = 2,
+		/// <summary>
+		/// Persons one and two are friends.
+		/// </summary>
+		Friends = FriendRequestSent | FriendRequestRecieved
+	}
+}
+
