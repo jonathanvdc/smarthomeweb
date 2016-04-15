cbdfbf7d4483b417719d5bf894938fbd60cb69d2
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-15 14:00:07 +0200

    Created a REST API: /api/sensors/at-location/{id}

--- backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiSensorsModule.cs
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiSensorsModule.cs
@@ -14,6 +14,7 @@ namespace SmartHomeWeb.Modules.API
             ApiGet("/", (_, dc) => dc.GetSensorsAsync());
             ApiGet("/{id}/", (p, dc) => dc.GetSensorByIdAsync((int)p["id"]));
 			ApiGet("/by-tag/{tag}", (p, dc) => dc.GetSensorsByTagAsync((string)p["tag"]));
+			ApiGet("/at-location/{id}", (p, dc) => dc.GetSensorsAtLocationAsync((int)p["id"]));
 
             ApiPost<List<SensorData>, object>("/", (_, items, dc) => dc.InsertSensorAsync(items));
         }
