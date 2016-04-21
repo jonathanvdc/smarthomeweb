fd8c68b93c51748a14ac3356bcf939d70d6abe21
Mauris Van Hauwe <mauris.van.hauwe@gmail.com>
2016-04-21 14:01:44 +0200

    Remove unused scripts and text files
    Either init-db.py or the Google Docs obsolete all of these.

--- ElecSim/init.bat
+++
@@ -1,7 +0,0 @@
-python3 main.py --mode=configure --no_house_holds=1 --output=MY_CONFIGURE.json
-
-ECHO off
-FOR /L %%A IN (1,1,1) DO python3 main.py --mode=generate --config_file=MY_CONFIGURE.json --household=%%A --from=2016-03-24T16:00 --to=2016-03-24T16:30 --output=MY_FILE%%A.csv 
-FOR /L %%A IN (1,1,1) DO python3 parsecsv.py -h %%A -i MY_FILE%%A.csv -o MY_FILE%%A.json
-
-FOR /L %%A IN (1,1,1) DO del MY_FILE%%A.csv
