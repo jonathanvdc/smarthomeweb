07f7017d786d18d3d8c3688d5be894e634709585
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-22 21:37:54 +0200

    Added a comment, fixed an API address

--- backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiFrozenModule.cs
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiFrozenModule.cs
@@ -4,11 +4,16 @@ namespace SmartHomeWeb.Modules.API
 {
 	public class ApiFrozenModule : ApiModule
 	{
-		public ApiFrozenModule() : base("/frozen")
+		public ApiFrozenModule() : base("api/frozen")
 		{
 			ApiGet("/{timestamp}", (p, dc) => dc.IsFrozenAsync((DateTime)p["timestamp"]));
 
+			// PUT, because freezing is idempotent
 			Put["/{start}/{end}", true] = Ask((p, dc) => dc.FreezeAsync((DateTime)p["start"], (DateTime)p["end"]));
+
+			// There is no DELETE/unfreeze request. This is by design, as freezing is supposed
+			// to guarantee the database's integrity. Unfreezing would therefore destroy the 
+			// database's integrity. We absolutely don't want that.
 		}
 	}
 }
