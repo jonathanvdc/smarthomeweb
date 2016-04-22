21139e7617696ad7c46908995cf451150ece0e18
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-22 21:32:30 +0200

    Created a /frozen REST API

---
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/API/ApiFrozenModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmartHomeWeb.Modules.API
+{
+	public class ApiFrozenModule : ApiModule
+	{
+		public ApiFrozenModule() : base("/frozen")
+		{
+			ApiGet("/{timestamp}", (p, dc) => dc.IsFrozenAsync((DateTime)p["timestamp"]));
+
+			Put["/{start}/{end}", true] = Ask((p, dc) => dc.FreezeAsync((DateTime)p["start"], (DateTime)p["end"]));
+		}
+	}
+}
+
