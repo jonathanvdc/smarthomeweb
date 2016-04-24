cb16f3f937b9d578ec665618a94398ff08ad8dd1
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-24 22:14:14 +0200

    Fixed a bug

--- backend/SmartHomeWeb/SmartHomeWeb/Modules/SmartHomeWebModule.cs
+++ backend/SmartHomeWeb/SmartHomeWeb/Modules/SmartHomeWebModule.cs
@@ -168,7 +168,7 @@ namespace SmartHomeWeb.Modules
 						// Empty names are silly, and we don't allow them.
 						ViewBag.Error = TextResources.EmptyNameError;
 					}
-					else if (name != editLoc.Data.Name && dc.GetLocationByNameAsync(name) != null)
+					else if (name != editLoc.Data.Name && await dc.GetLocationByNameAsync(name) != null)
 					{
 						// Whoops. Name already exists.
 						ViewBag.Error = string.Format(TextResources.LocationAlreadyExistsError, name);
