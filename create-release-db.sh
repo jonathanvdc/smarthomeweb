aed8667a6468381e524ba6f05563229ceb99759b
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-01 20:13:16 +0200

    Created a script that produces a release database

---
+++ create-release-db.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python init-db.py 720
+cp backend/database/smarthomeweb.db backend/database/smarthomeweb-720.db
+python compact.py measurements $(date -I --date "14 days ago") $(date -I --date "7 days ago")
+python compact.py hour-average $(date -I --date "2 months ago") $(date -I --date "14 days ago")
+python compact.py day-average $(date -I --date "2 years ago") $(date -I --date "2 months ago")
+python vacuum.py
